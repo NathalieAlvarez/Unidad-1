d91eca9ce9990fc1aff77e9f54b2a69f4a1f8167
Nathalie Álvarez Hernández <konathie@gmail.com>
2021-08-30 00:33:13 -0500

    nombre y email guardados

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoVSC.Sintaxis3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("estoy aprendiendo a hacer repositorios, no te burles, por favor");
+        }
+    }
+}
